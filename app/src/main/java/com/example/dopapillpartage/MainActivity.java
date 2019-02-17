30af43aae8d80e51ebba821fdd4cdf70449047b2
randrianarimanana <vero-hanitra-mariah.hanitra@etu.univ-grenoble-alpes.fr>
2019-02-17 19:57:19 +0100

    Trois interfaces : premierepage pour differencier "s'inscrire" et "se connecter" / pagedeconnexion / page d'inscription Trois activités : mainActivity / SinscrireActivity / ConnexionActivity J'ai pas d'erreur, mais comme je n'ai pas runné sur un telephone je ne suis pas sure que ca marche Il faudra relier à la base de données

--- app/src/main/java/com/example/dopapillpartage/MainActivity.java
+++ app/src/main/java/com/example/dopapillpartage/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.dopapillpartage;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_seconnecter, btn_sinscrire;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        btn_seconnecter = (Button) findViewById(R.id.boutonseconnecter);
+        btn_sinscrire = (Button) findViewById(R.id.boutonsinscrire);
+
+        btn_sinscrire.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SinscrireActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_seconnecter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ConnexionActivity.class);
+                startActivity(intent);
+            }
+        });
+
 }
+
+}
+
